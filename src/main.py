378d52e044a8b7cd06310947e546f6edf3dbdd6b
asakel <asakellaropoulos@iit.demokritos.gr>
2025-11-19 15:38:54 +0200

    fixed config

--- src/main.py
+++ src/main.py
@@ -4,12 +4,11 @@
 '''
 from app.utils.log import get_app_logger 
 from app import _app
-from app.utils.app_config import NEF_BASE_URL,PCF_BASE_URL
+from app.utils.app_config import PCF_BASE_URL
 
 logger = get_app_logger()
 
 logger.info('*** NEF-AsSessionWithQos **')
-logger.info(f'NEF Base URL: {NEF_BASE_URL}')
 logger.info(f'PCF Base URL: {PCF_BASE_URL}')
 
 
